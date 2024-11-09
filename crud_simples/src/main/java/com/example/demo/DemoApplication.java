f8b2857ac86ad2035835f593e075ad0868fe94a1
nicolly_jesus <128162519+nicollyjesus@users.noreply.github.com>
2024-11-09 13:35:55 -0300

    crud test

--- crud_simples/src/main/java/com/example/demo/DemoApplication.java
+++ crud_simples/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,3 @@
 package com.example.demo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
 public interface ProductRepository extends JpaRepository<Product, String> {}
